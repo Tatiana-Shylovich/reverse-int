5e2717f998abbd48f8e2b0ba4f7731af4d50963b
tatiana <miss.lacrima.ts@gmail.com>
2022-08-01 21:55:09 +0300

    feat: done with this

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
+  return n.toString().match(/\d/g).reverse().join('')
+
+  // return n.toString().match(/[0-9]/g).reverse().join('')
+
+  // return Math.abs(n).toString().split('').reverse().join('')
+
+  // return Array.from(Math.abs(n).toString()).reverse().join('')
+
+  // return [...Math.abs(n).toString()].reverse().join('')
   
+  /* const posNum = n.toString().startsWith('-') ? n.toString().slice(1) : n.toString()
+  // const posNum = n.toString().startsWith('-') ? n.toString().replace('-', '') : n.toString()
+  let newStr = ''
+  for (let i = posNum.length - 1; i >= 0; i--) {
+    newStr = newStr + posNum[i]
+  }
+  return Number(newStr) */
 }
