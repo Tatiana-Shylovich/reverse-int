f78eb8aed597162333df0e656671f6a078e907d5
tatiana <miss.lacrima.ts@gmail.com>
2022-08-02 03:21:24 +0300

    refactor: code formatting and commenting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 module.exports = function reverse (n) {
-  return n.toString().match(/\d/g).reverse().join('')
+  return +n.toString().match(/\d/g).reverse().join('')
 
+  // use + to covert string to number
+  
   // return n.toString().match(/[0-9]/g).reverse().join('')
 
   // return Math.abs(n).toString().split('').reverse().join('')
@@ -9,11 +11,13 @@ module.exports = function reverse (n) {
 
   // return [...Math.abs(n).toString()].reverse().join('')
   
-  /* const posNum = n.toString().startsWith('-') ? n.toString().slice(1) : n.toString()
+ /* const posNum = n.toString().startsWith('-') ? n.toString().slice(1) : n.toString()
   // const posNum = n.toString().startsWith('-') ? n.toString().replace('-', '') : n.toString()
   let newStr = ''
   for (let i = posNum.length - 1; i >= 0; i--) {
     newStr = newStr + posNum[i]
   }
-  return Number(newStr) */
-}
+  return +newStr
+  // return parseInt(newStr, 10)
+  // return Number(newStr) */
+} 
